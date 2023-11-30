4e1a9e3f2d77b04769e32cc8493f1d3c2df46515
Yohanes Naibaho <yohanespratama303@gmail.com>
2023-11-30 12:38:06 +0700

    create header homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,35 @@
+import { Container, Row, Col } from "react-bootstrap";
+import HeroImage from "../assets/img/hero.png";
+
 const HomePage = () => {
   return (
     <div className="homepage">
-      <header className="w-100 min-vh-100"></header>
-      <div className="2-100 min-vh-100"></div>
+      <header className="w-100 min-vh-100 d-flex align-items-center">
+        <Container>
+          <Row className="header-box d-flex align-items-center">
+            <Col lg="6">
+              <h1 className="mb-4">
+                Temukan <br /> <span>Bakat Kreatifmu</span> <br />
+                Bersama Kami!
+              </h1>
+              <p className="mb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+                eveniet nostrum porro libero ducimus nobis.
+              </p>
+              <button className="btn btn-danger btn-lg rounded-1 me-2 mb-xs-0 mb-2">
+                Lihat Kelas
+              </button>
+              <button className="btn btn-outline-danger btn-lg rounded-1 mb-xs-0 mb-2">
+                Lihat Promo
+              </button>
+            </Col>
+            <Col lg="6" className="pt-lg-0 pt-5">
+              <img src={HeroImage} alt="hero-img" />
+            </Col>
+          </Row>
+        </Container>
+      </header>
+      <div className="kelas w-100 min-vh-100"></div>
     </div>
   );
 };
