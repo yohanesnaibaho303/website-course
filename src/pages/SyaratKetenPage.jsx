37a1fafee05ff023d4a2ec669a79bed60d7831a0
Yohanes Naibaho <yohanespratama303@gmail.com>
2023-12-01 16:41:47 +0700

    create kelas,snk and testimonial page

--- src/pages/SyaratKetenPage.jsx
+++ src/pages/SyaratKetenPage.jsx
@@ -1,5 +1,136 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+import Faq from "../components/Faq";
+
 const SyaratKetenPage = () => {
-  return <div>SyaratKetenPage</div>;
+  return (
+    <div className="syarat-ketentuan-page">
+      <div className="syarat-ketentuan min-vh-100">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="fw-bold text-center mb-2">Syarat & Ketentuan</h1>
+              <p className="text-center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius,
+                veritatis!
+              </p>
+            </Col>
+          </Row>
+          <Row className="pt-5">
+            <Col>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Cupiditate, impedit cum voluptate voluptatum odit, tenetur
+                laudantium totam inventore asperiores ab ex. Error natus
+                architecto nostrum in doloremque, ad beatae quibusdam,
+                laudantium tenetur atque earum ratione est dolores deleniti. Rem
+                ratione totam dolorum. Ad, error. Deleniti, debitis amet! Magni,
+                provident iste.
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">1. Lorem</h4>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Impedit vel, excepturi quia quo debitis, officiis ipsum
+                repellendus pariatur exercitationem quos ducimus? Voluptate
+                ratione veniam explicabo dolores, libero dolor eos beatae, saepe
+                distinctio animi incidunt sunt, delectus atque non nihil
+                doloribus?
+              </p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum,
+                esse eius? Iusto animi quisquam sed cumque provident quo omnis
+                voluptate ipsam, explicabo quam quidem molestiae et ex minima
+                delectus eum! Optio neque libero minima, odio, debitis dicta ex
+                exercitationem dolorem, sed vero non quo tenetur. Quam dicta
+                eum, unde, velit sed reprehenderit similique atque laboriosam
+                amet debitis cumque, laudantium vitae.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequuntur est modi veritatis! Incidunt vitae excepturi
+                quaerat culpa rerum voluptas reprehenderit, consectetur ullam
+                magnam. Suscipit, fugiat? Deserunt totam labore praesentium
+                dolor dicta accusantium nam, ut deleniti explicabo, molestiae ea
+                iste! Libero rem voluptatibus aperiam nobis ratione ut illum,
+                impedit pariatur officiis doloribus. Doloremque, libero. Nulla
+                et ratione sed velit dolorem saepe vel ut facere sint sequi
+                impedit mollitia deleniti sapiente, nostrum voluptate
+                dignissimos, aliquid necessitatibus repellat? Vero sapiente ab
+                dicta laboriosam.
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">2. Ipsum</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Maiores eos perferendis, quia quis velit, pariatur ex porro
+                voluptate similique praesentium harum nostrum at, quaerat esse
+                distinctio nihil atque vitae! Minima quis amet quam obcaecati,
+                voluptate voluptas. Atque provident earum aliquam facilis minima
+                dicta tempora culpa laborum et placeat, architecto autem
+                pariatur eos nihil illo sit ad maiores iure blanditiis ullam?
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+                iusto doloribus quia explicabo, vitae veritatis quod reiciendis
+                magnam sequi necessitatibus soluta dignissimos iure nihil
+                expedita dolor iste voluptatibus, consequatur fugiat. Tenetur
+                minima esse doloremque earum sequi adipisci laboriosam, corrupti
+                natus laudantium quisquam suscipit hic cumque! Sapiente tempore
+                cumque soluta debitis reiciendis est dolore, quaerat possimus
+                voluptatibus. Officia fugit ipsa architecto laborum culpa,
+                dolorum placeat asperiores consequatur vel consequuntur
+                perspiciatis quaerat quo! Nostrum sint autem minus nemo pariatur
+                quos, voluptates corporis?
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">3. Dolor</h4>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Impedit vel, excepturi quia quo debitis, officiis ipsum
+                repellendus pariatur exercitationem quos ducimus? Voluptate
+                ratione veniam explicabo dolores, libero dolor eos beatae, saepe
+                distinctio animi incidunt sunt, delectus atque non nihil
+                doloribus?
+              </p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum,
+                esse eius? Iusto animi quisquam sed cumque provident quo omnis
+                voluptate ipsam, explicabo quam quidem molestiae et ex minima
+                delectus eum! Optio neque libero minima, odio, debitis dicta ex
+                exercitationem dolorem, sed vero non quo tenetur. Quam dicta
+                eum, unde, velit sed reprehenderit similique atque laboriosam
+                amet debitis cumque, laudantium vitae.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequuntur est modi veritatis! Incidunt vitae excepturi
+                quaerat culpa rerum voluptas reprehenderit, consectetur ullam
+                magnam. Suscipit, fugiat? Deserunt totam labore praesentium
+                dolor dicta accusantium nam, ut deleniti explicabo, molestiae ea
+                iste! Libero rem voluptatibus aperiam nobis ratione ut illum,
+                impedit pariatur officiis doloribus. Doloremque, libero. Nulla
+                et ratione sed velit dolorem saepe vel ut facere sint sequi
+                impedit mollitia deleniti sapiente, nostrum voluptate
+                dignissimos, aliquid necessitatibus repellat? Vero sapiente ab
+                dicta laboriosam.
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <Faq />
+    </div>
+  );
 };
 
 export default SyaratKetenPage;
--- src/pages/TestimonialPage.jsx
+++ src/pages/TestimonialPage.jsx
@@ -1,9 +1,44 @@
+import { Container, Row, Col } from "react-bootstrap";
 
+import { testimonial } from "../data";
+import Faq from "../components/Faq";
 
 const TestimonialPage = () => {
   return (
-    <div>TestimonialPage</div>
-  )
-}
+    <div className="testimonial-page">
+      <div className="testimonial">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="text-center fw-bold">Semua Testimonial</h1>
+              <p className="text-center">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Magnam, facilis.
+              </p>
+            </Col>
+          </Row>
+          <Row className="row-cols-lg-3 row-cols-1">
+            {testimonial.map((data) => {
+              return (
+                <Col key={data.id} className="mb-5">
+                  <p className="desc shadow-sm">{data.desc}</p>
+                  <div className="people">
+                    <img src={data.image} alt="poto-swipper" />
+                    <div>
+                      <h5 className="mb-1">{data.name}</h5>
+                      <p className="m-0 fw-bold">{data.skill}</p>
+                    </div>
+                  </div>
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </div>
 
-export default TestimonialPage
+      <Faq />
+    </div>
+  );
+};
+
+export default TestimonialPage;
